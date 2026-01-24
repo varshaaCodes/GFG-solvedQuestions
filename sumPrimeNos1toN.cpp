f4b924ef25e3f98671c09e4a968e666ed2b9c2e8
varshaaCodes <91754632+varshaaCodes@users.noreply.github.com>
2026-01-24 20:57:59 +0530

    Add solution to sum prime numbers from 1 to n
    Implement a class to compute the sum of prime numbers up to n.

---
+++ sumPrimeNos1toN.cpp
@@ -0,0 +1,31 @@
+//Given a positive integer n, compute and return the sum of all prime numbers between 1 and n (inclusive).
+
+//A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.  
+
+
+class Solution {
+  public:
+  bool isprime(int num){
+     if(num<2)return false;
+        if(num==2)return true;
+        if(num%2==0)return false;
+        for(int i=3;i<=sqrt(num);i+=2){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+  }
+    int prime_Sum(int n) {
+        // Code here
+      long long sum=0;
+      for(int i=2;i<=n;i++){
+          if(isprime(i)){
+              sum+=i;
+          }
+      }
+      return sum;
+        
+        
+    }
+};
