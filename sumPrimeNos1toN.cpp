771aaa42eb783d9cbf2be56808ec5c6f9328d1f7
varshaaCodes <91754632+varshaaCodes@users.noreply.github.com>
2026-01-24 21:00:14 +0530

    Add comment with problem link for clarity
    Added a comment with a link to the problem statement on GeeksforGeeks.

--- sumPrimeNos1toN.cpp
+++ sumPrimeNos1toN.cpp
@@ -1,6 +1,7 @@
 //Given a positive integer n, compute and return the sum of all prime numbers between 1 and n (inclusive).
 
 //A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.  
+//Gfg link- https://www.geeksforgeeks.org/problems/sum-of-all-prime-numbers-between-1-and-n4404/1
 
 
 class Solution {
